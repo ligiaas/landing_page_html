739865792873717dec6aa96768a653d908d00805
Ligia de Almeida Silva <ligia@waveweb.com.br>
2018-06-19 21:42:51 -0300

    feito testimonial com slick carousel

--- www/script.js
+++ www/script.js
@@ -1,26 +1,9 @@
 $(document).ready(function(){
-    // Activate Carousel
-    $("#testimonial-carousel").carousel({interval: 500});
-    
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#testimonial-carousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#testimonial-carousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#testimonial-carousel").carousel(2);
-    });
-    $(".item4").click(function(){
-        $("#testimonial-carousel").carousel(3);
-    });
-    
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#testimonial-carousel").carousel("prev");
-    });
-    $(".right").click(function(){
-        $("#testimonial-carousel").carousel("next");
-    });
+  $('.test__carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    adaptiveHeight: true
+  });
 });
