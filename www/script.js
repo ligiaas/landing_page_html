30249768603450a8135e00d3c324d4f684d46215
Ligia de Almeida Silva <ligia@waveweb.com.br>
2018-06-21 13:05:02 -0300

    finalizado subscribe, iniciado footer -> falta ajustar responsividade.

--- www/script.js
+++ www/script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    adaptiveHeight: true
+    autoplaySpeed: 5000,
   });
 });
